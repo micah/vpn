6b0514ae59a39ff1cb9a49cb0fb48723f9eec5d7
ankit <ankitgusai21@gmail.com>
2022-07-31 23:48:59 -0400

    -Landing screen -Theme created for Day/night -few common styles

---
+++ app/src/main/java/com/example/torwitharti/ui/home/ConnectFragment.kt
@@ -0,0 +1,39 @@
+package com.example.torwitharti.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.torwitharti.databinding.FragmentConnectBinding
+
+class ConnectFragment : Fragment() {
+
+    private var _binding: FragmentConnectBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentConnectBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
