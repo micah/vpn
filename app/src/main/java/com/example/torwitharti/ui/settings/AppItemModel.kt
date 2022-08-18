cc87f981fc96d1e41438285441d16adf0336d3cb
cyberta <cyberta@riseup.net>
2022-08-19 01:08:51 +0200

    query all packages which are allowed to use internet connectivity, flag browsers as such

--- app/src/main/java/com/example/torwitharti/ui/settings/AppItemModel.kt
+++ app/src/main/java/com/example/torwitharti/ui/settings/AppItemModel.kt
@@ -2,9 +2,10 @@ package com.example.torwitharti.ui.settings
 
 import android.graphics.drawable.Drawable
 
-class AppItemModel (name: String, appId: String, icon: Drawable?, isRoutingEnabled: Boolean, isBrowserApp: Boolean, hasTorSocksSupport: Boolean) {
+class AppItemModel (name: String, appId: String, uid: Int, icon: Drawable?, isRoutingEnabled: Boolean, isBrowserApp: Boolean, hasTorSocksSupport: Boolean) {
     val name: String
     val appId: String
+    val uid: Int
     val icon: Drawable?
     val isRoutingEnabled: Boolean
     val isBrowserApp: Boolean
@@ -13,6 +14,7 @@ class AppItemModel (name: String, appId: String, icon: Drawable?, isRoutingEnabl
     init {
         this.name = name
         this.appId = appId
+        this.uid = uid
         this.icon = icon
         this.isRoutingEnabled = isRoutingEnabled
         this.isBrowserApp = isBrowserApp
--- app/src/main/java/com/example/torwitharti/utils/AppManager.kt
+++ app/src/main/java/com/example/torwitharti/utils/AppManager.kt
@@ -1,26 +1,101 @@
 package com.example.torwitharti.utils
 
+import android.Manifest.permission.INTERNET
 import android.content.Context
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.DrawableCompat
-import com.example.torwitharti.R
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.content.pm.ResolveInfo
+import android.net.Uri
+import android.os.Build
+import android.text.TextUtils
+import androidx.annotation.WorkerThread
 import com.example.torwitharti.ui.settings.AppItemModel
+
 
 class AppManager(context: Context) {
     private val context: Context
+    private val preferenceHelper: PreferenceHelper
+    private val pm: PackageManager
     init {
         this.context = context
+        this.preferenceHelper = PreferenceHelper(context)
+        this.pm = context.packageManager
     }
-    fun queryInstalledApps() : List<AppItemModel>  {
-        // TODO: implement me!
-        val arrayList = mutableListOf<AppItemModel>().apply {
 
+    @WorkerThread
+    fun queryInstalledApps() : List<AppItemModel> {
+        val installedPackages = pm.getInstalledApplications(PackageManager.GET_META_DATA)
+        val installedBrowsers = getInstalledBrowserPackages()
+        var androidSystemUid = 0
+        val apps = mutableListOf<AppItemModel>()
 
-            val drawable = ContextCompat.getDrawable(context, R.drawable.ic_dummy_app)
-            add(AppItemModel("Bitmask", "se.leap.bitmaskclient", drawable, false, false, false))
-            add(AppItemModel("RiseupVPN", "se.leap.riseupvpn", drawable, false, false, false))
-            add(AppItemModel("F-Droid", "org.fdroid.fdroid", drawable, false, false, true))
+        // only Add apps using which are allowed to use internet
+        try {
+            val system = pm.getApplicationInfo("android", PackageManager.GET_META_DATA)
+            androidSystemUid = system.uid
+            createAppItemModel(system)?.also {
+                apps.add(it)
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
         }
-        return arrayList
+
+        for (appInfo in installedPackages) {
+            if (pm.checkPermission(INTERNET, appInfo.packageName) == PERMISSION_GRANTED && appInfo.uid != androidSystemUid) {
+                createAppItemModel(appInfo, installedBrowsers)?.also {
+                    apps.add(it)
+                }
+            }
+        }
+
+        return apps.sortedBy { it.name }
     }
+
+    private fun createAppItemModel(applicationInfo: ApplicationInfo, browserPackages: List<String> = listOf()): AppItemModel? {
+        var appName = applicationInfo.loadLabel(pm) as? String
+        if (TextUtils.isEmpty(appName))
+            appName = applicationInfo.packageName
+
+        appName?.also {
+            val appIcon = applicationInfo.loadIcon(pm)
+            val appUID = applicationInfo.uid
+            val packageName = applicationInfo.packageName
+            return AppItemModel(
+                it,
+                packageName,
+                appUID,
+                appIcon,
+                false,
+                browserPackages.contains(packageName),
+                false)
+        }
+
+        return null
+    }
+
+    private fun  getInstalledBrowserPackages() :  List<String> {
+            val queryBrowsersIntent = Intent()
+                .setAction(Intent.ACTION_VIEW)
+                .addCategory(Intent.CATEGORY_BROWSABLE)
+                .setData(Uri.parse("https://"))
+
+            val installedBrowsers = mutableListOf<ResolveInfo>()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                installedBrowsers.addAll(
+                    pm.queryIntentActivities(
+                        queryBrowsersIntent,
+                        PackageManager.MATCH_ALL
+                    )
+                )
+            } else {
+                installedBrowsers.addAll(pm.queryIntentActivities(queryBrowsersIntent, PackageManager.MATCH_DEFAULT_ONLY))
+            }
+
+            val browserPackageNames = mutableListOf<String>()
+            for (browser in installedBrowsers) {
+                browserPackageNames.add(browser.activityInfo.packageName)
+            }
+            return browserPackageNames
+        }
 }
