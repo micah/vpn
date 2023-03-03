3d8b6fc56abc2bc2bf60baf773cf6e9d981cd166
cyberta <cyberta@riseup.net>
2023-03-03 02:03:56 +0100

    clean up code, fix lint issues

--- app/src/main/java/com/example/torwitharti/ui/configure/view/ConfigItemView.kt
+++ app/src/main/java/com/example/torwitharti/ui/configure/view/ConfigItemView.kt
@@ -3,20 +3,14 @@ package com.example.torwitharti.ui.configure.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.torwitharti.R
 import com.example.torwitharti.databinding.ConfigItemViewBinding
 
 /**
- * TODO: document your custom view class.
+ * Configuration entry used in ConfigureFragment
  */
 class ConfigItemView : ConstraintLayout {
-
-    private lateinit var imageView: AppCompatImageView
-    private lateinit var titleView: AppCompatTextView
-    private lateinit var subtitleView: AppCompatTextView
 
     private val binding by lazy {
         ConfigItemViewBinding.inflate(LayoutInflater.from(context), this, true)
@@ -43,14 +37,10 @@ class ConfigItemView : ConstraintLayout {
         val a = context.obtainStyledAttributes(
             attrs, R.styleable.ConfigItemView, defStyle, 0
         )
-
-        titleView =  binding.tvTitle
-        imageView = binding.ivIcon
-        subtitleView = binding.tvSubtitle
-        titleView.text = a.getString(R.styleable.ConfigItemView_text)
-        subtitleView.text = a.getString(R.styleable.ConfigItemView_secondaryText)
-
-        imageView.setImageDrawable(a.getDrawable(R.styleable.ConfigItemView_drawable))
+        binding.tvTitle.text = a.getString(R.styleable.ConfigItemView_text)
+        binding.tvSubtitle.text = a.getString(R.styleable.ConfigItemView_secondaryText)
+        binding.ivIcon.setImageDrawable(a.getDrawable(R.styleable.ConfigItemView_drawable))
+        a.recycle()
     }
 
 }
