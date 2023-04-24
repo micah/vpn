09bcfe62bec617a05a2e31161cd18a5d51d00eb2
cyBerta <cyberta@riseup.net>
2023-04-24 18:20:12 +0200

    load app icons asynchronously as drawables instead of bitmaps

--- app/src/main/java/com/example/torwitharti/ui/glide/AppIconDataFetcher.kt
+++ app/src/main/java/com/example/torwitharti/ui/glide/AppIconDataFetcher.kt
@@ -6,6 +6,7 @@ import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import com.bumptech.glide.Priority
+import com.bumptech.glide.Registry
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.data.DataFetcher
 
@@ -14,18 +15,22 @@ class AppIconDataFetcher internal constructor(
     context: Context,
     private val mModel: ApplicationInfoModel
 ) :
-    DataFetcher<Bitmap> {
+    DataFetcher<Drawable> {
     private val mContext: Context
 
     init {
         mContext = context
     }
 
-    override fun loadData(priority: Priority, callback: DataFetcher.DataCallback<in Bitmap>) {
-        val applicationInfo = mContext.packageManager.getApplicationInfo(mModel.toString(), 0)
-        val icon: Drawable = mContext.packageManager.getApplicationIcon(applicationInfo)
-        val bitmap = drawableToBitmap(icon)
-        callback.onDataReady(bitmap)
+    override fun loadData(priority: Priority, callback: DataFetcher.DataCallback<in Drawable>) {
+        try {
+            val applicationInfo = mContext.packageManager.getApplicationInfo(mModel.toString(), 0)
+            val icon: Drawable = mContext.packageManager.getApplicationIcon(applicationInfo)
+            callback.onDataReady(icon)
+        } catch (e: Registry.NoResultEncoderAvailableException) {
+            e.printStackTrace()
+            // TODO: we need to set a default icn
+        }
     }
 
     override fun cleanup() {
@@ -36,39 +41,11 @@ class AppIconDataFetcher internal constructor(
         // Empty Implementation
     }
 
-    override fun getDataClass(): Class<Bitmap> {
-        return Bitmap::class.java
+    override fun getDataClass(): Class<Drawable> {
+        return Drawable::class.java
     }
 
     override fun getDataSource(): DataSource {
         return DataSource.LOCAL
-    }
-
-    private fun drawableToBitmap(drawable: Drawable): Bitmap? {
-        if (drawable is BitmapDrawable) {
-            drawable.bitmap?.also {
-                return it
-            }
-        }
-        val bitmap: Bitmap? = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
-            Bitmap.createBitmap(
-                80,
-                80,
-                Bitmap.Config.ARGB_8888
-            )
-        } else {
-            Bitmap.createBitmap(
-                drawable.intrinsicWidth,
-                drawable.intrinsicHeight,
-                Bitmap.Config.ARGB_8888
-            )
-        }
-
-        bitmap?.also {
-            val canvas = Canvas(it)
-            drawable.setBounds(0, 0, canvas.width, canvas.height)
-            drawable.draw(canvas)
-        }
-        return bitmap
     }
 }
--- app/src/main/java/com/example/torwitharti/ui/glide/AppIconModelLoaderFactory.kt
+++ app/src/main/java/com/example/torwitharti/ui/glide/AppIconModelLoaderFactory.kt
@@ -2,20 +2,21 @@ package com.example.torwitharti.ui.glide
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import com.bumptech.glide.load.model.ModelLoader
 import com.bumptech.glide.load.model.ModelLoaderFactory
 import com.bumptech.glide.load.model.MultiModelLoaderFactory
 
 
 class AppIconModelLoaderFactory internal constructor(context: Context) :
-    ModelLoaderFactory<ApplicationInfoModel, Bitmap> {
+    ModelLoaderFactory<ApplicationInfoModel, Drawable> {
     private val mContext: Context
 
     init {
         mContext = context
     }
 
-    override fun build(multiFactory: MultiModelLoaderFactory): ModelLoader<ApplicationInfoModel, Bitmap> {
+    override fun build(multiFactory: MultiModelLoaderFactory): ModelLoader<ApplicationInfoModel, Drawable> {
         return AppIconModelLoader(mContext)
     }
 
