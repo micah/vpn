0051ca4d4b5061d0a4d3f0a322266646cf821b47
cyBerta <cyberta@riseup.net>
2023-04-27 15:27:27 +0200

    show connection stats updates in connect screen

--- app/src/main/java/com/example/torwitharti/ui/home/model/ConnectFragmentViewModel.kt
+++ app/src/main/java/com/example/torwitharti/ui/home/model/ConnectFragmentViewModel.kt
@@ -5,12 +5,14 @@ import android.content.Intent
 import android.net.VpnService
 import android.os.Handler
 import android.os.Looper
+import android.text.format.Formatter
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.*
 import com.example.torwitharti.BuildConfig
 import com.example.torwitharti.R
 import com.example.torwitharti.vpn.ConnectionState
 import com.example.torwitharti.vpn.ConnectionState.*
+import com.example.torwitharti.vpn.DataUsage
 import com.example.torwitharti.vpn.VpnServiceCommand
 import com.example.torwitharti.vpn.VpnStatusObservable
 import kotlinx.coroutines.flow.*
@@ -26,6 +28,48 @@ class ConnectFragmentViewModel(application: Application) : AndroidViewModel(appl
 
     private val _prepareVpn = MutableLiveData<Intent?>()
     val prepareVpn: LiveData<Intent?> = _prepareVpn
+    private val dataUsage = VpnStatusObservable.dataUsage.asFlow()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Lazily,
+            initialValue = DataUsage()
+        )
+
+    val dataUsageDownstream: StateFlow<String> = dataUsage.map { data ->
+        val received = Formatter.formatFileSize(application, data.downstreamData)
+        return@map application.getString(R.string.stats_down, received)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Lazily,
+        initialValue = ""
+    )
+
+    val dataUsageUpstream: StateFlow<String> = dataUsage.map { data ->
+        val sent = Formatter.formatFileSize(application, data.upstreamData)
+        return@map application.getString(R.string.stats_up, sent)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Lazily,
+        initialValue = ""
+    )
+
+    val dataUsageDiffDownstream: StateFlow<String> = dataUsage.map { data ->
+        val receivedDiff = Formatter.formatFileSize(application, data.downstreamDataPerSec)
+        return@map application.getString(R.string.stats_delta, receivedDiff)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Lazily,
+        initialValue = ""
+    )
+
+    val dataUsageDiffUpstream: StateFlow<String> = dataUsage.map { data ->
+        val receivedDiff = Formatter.formatFileSize(application, data.upstreamDataPerSec)
+        return@map application.getString(R.string.stats_delta, receivedDiff)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Lazily,
+        initialValue = ""
+    )
 
     val connectionState = VpnStatusObservable.statusLiveData.asFlow()
         .stateIn(
