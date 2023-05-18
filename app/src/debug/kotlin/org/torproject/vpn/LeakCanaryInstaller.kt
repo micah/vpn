edf9e5612066736da6448c246ebc1861d441c504
cyBerta <cyberta@riseup.net>
2023-05-18 14:33:12 +0200

    add leak canary to debug builds (if not tests)

--- app/src/debug/kotlin/org/torproject/vpn/LeakCanaryInstaller.kt
+++ app/src/debug/kotlin/org/torproject/vpn/LeakCanaryInstaller.kt
@@ -1,4 +1,57 @@
 package org.torproject.vpn
 
-class LeakCanaryInstaller {
+import android.app.Application
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import leakcanary.AppWatcher
+
+
+class LeakCanaryInstaller : ContentProvider() {
+    override fun onCreate(): Boolean {
+        if (!isTest) {
+            AppWatcher.manualInstall(context!!.applicationContext as Application)
+        }
+        return false
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<String>?,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        sortOrder: String?
+    ): Cursor? {
+        return null
+    }
+
+    override fun getType(uri: Uri): String? {
+        return null
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        return 0
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<String>?
+    ): Int {
+        return 0
+    }
+
+    private val isTest: Boolean
+        get() = try {
+            Class.forName("org.junit.Test")
+            true
+        } catch (e: java.lang.Exception) {
+            false
+        }
 }
