9825e0ae0fda405a9dead3f1cfaaafe9f43c4f97
cyBerta <cyberta@riseup.net>
2024-02-01 19:52:20 +0100

    add subtitles with corrent connectivity state to tiles on Android Q+. Keep title label (main title) mainly for the app name. Open app in an error case

--- app/src/main/java/org/torproject/vpn/ui/quicksettings/TorVPNTileService.kt
+++ app/src/main/java/org/torproject/vpn/ui/quicksettings/TorVPNTileService.kt
@@ -3,15 +3,24 @@ package org.torproject.vpn.ui.quicksettings
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.Intent.ACTION_MAIN
 import android.graphics.drawable.Icon
+import android.os.Build
 import android.os.Handler
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import android.util.Log
 import androidx.lifecycle.Observer
 import org.torproject.vpn.MainActivity
+import org.torproject.vpn.MainActivity.Companion.ACTION_REQUEST_VPN_PERMISSON
 import org.torproject.vpn.R
 import org.torproject.vpn.vpn.ConnectionState
+import org.torproject.vpn.vpn.ConnectionState.CONNECTED
+import org.torproject.vpn.vpn.ConnectionState.CONNECTING
+import org.torproject.vpn.vpn.ConnectionState.CONNECTION_ERROR
+import org.torproject.vpn.vpn.ConnectionState.DISCONNECTED
+import org.torproject.vpn.vpn.ConnectionState.DISCONNECTING
+import org.torproject.vpn.vpn.ConnectionState.INIT
 import org.torproject.vpn.vpn.VpnServiceCommand
 import org.torproject.vpn.vpn.VpnStatusObservable
 
@@ -34,20 +43,30 @@ class TorVPNTileService : TileService() {
     }
 
     private fun onTileTap() {
-        if (VpnStatusObservable.isVPNActive()) {
-            VpnServiceCommand.stopVpn(this)
-        } else {
-            if (VpnServiceCommand.prepareVpn(this) == null) {
-                VpnServiceCommand.startVpn(this)
-                VpnStatusObservable.update(ConnectionState.CONNECTING)
-            } else {
-                // retry to prepare and start the VPN but with some UI to show possible errors
-                val startIntent = Intent(this.applicationContext, MainActivity::class.java)
-                startIntent.action = MainActivity.ACTION_REQUEST_VPN_PERMISSON
-                startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(startIntent)
+        when (VpnStatusObservable.statusLiveData.value) {
+            CONNECTING, CONNECTED -> {
+                VpnServiceCommand.stopVpn(this)
+            }
+            CONNECTION_ERROR -> {
+               startMainActivity(ACTION_MAIN)
+            }
+            else -> {
+                if (VpnServiceCommand.prepareVpn(this) == null) {
+                    VpnServiceCommand.startVpn(this)
+                    VpnStatusObservable.update(CONNECTING)
+                } else {
+                    startMainActivity(ACTION_REQUEST_VPN_PERMISSON)
+                }
             }
         }
+    }
+
+    private fun startMainActivity(action: String) {
+        // retry to prepare and start the VPN but with some UI to show possible errors
+        val startIntent = Intent(this.applicationContext, MainActivity::class.java)
+        startIntent.action = action
+        startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        this.applicationContext.startActivity(startIntent)
     }
 
     override fun onTileAdded() {
@@ -64,33 +83,48 @@ class TorVPNTileService : TileService() {
     private fun onStatusChanged(connectionState: ConnectionState?) {
         val t = qsTile ?: return
         Log.d("TILE_TOR_VPN", "CONNECTION STATE CHANGED: ${connectionState?.name}")
-        //TODO: add and update an icon
         when (connectionState) {
-            ConnectionState.INIT,
-            ConnectionState.DISCONNECTED -> {
+            INIT,
+            DISCONNECTED -> {
                 t.state = Tile.STATE_INACTIVE
-                t.label = getString(R.string.qs_connect)
+                t.label = getString(R.string.app_name)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    t.subtitle = getString(R.string.state_disconnected)
+                }
             }
-            ConnectionState.CONNECTING -> {
+            CONNECTING -> {
+                t.state =  Tile.STATE_ACTIVE
+                t.label = getString(R.string.app_name)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    t.subtitle = getString(R.string.state_connecting)
+                }
+            }
+            DISCONNECTING,
+            CONNECTED -> {
                 t.state = Tile.STATE_ACTIVE
-                t.label = getString(R.string.qs_connecting)
+                t.label = getString(R.string.app_name)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    t.subtitle = getString(R.string.state_connected)
+                }
             }
-            ConnectionState.DISCONNECTING,
-            ConnectionState.CONNECTED -> {
+            CONNECTION_ERROR -> {
                 t.state = Tile.STATE_ACTIVE
-                t.label = getString(R.string.qs_disconnect)
-            }
-            ConnectionState.CONNECTION_ERROR -> {
-                t.state = Tile.STATE_UNAVAILABLE
-                t.label = getString(R.string.qs_error)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    t.label = getString(R.string.app_name)
+                    t.subtitle = getString(R.string.qs_error)
+                } else {
+                    t.label = getString(R.string.qs_show_error)
+                }
             }
             else -> {
                 t.state = Tile.STATE_UNAVAILABLE
-                t.label = getString(R.string.qs_connect)
+                t.label = getString(R.string.app_name)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    t.subtitle = ""
+                }
             }
         }
         t.icon = Icon.createWithResource(this, R.drawable.ic_launcher_foreground_qs)
-
         t.updateTile()
     }
 
