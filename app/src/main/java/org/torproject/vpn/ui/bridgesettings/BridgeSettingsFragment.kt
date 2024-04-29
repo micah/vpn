54775b4fe83857cb28a5089f58cd1e95ae5691f5
cyBerta <cyberta@riseup.net>
2024-04-29 10:38:17 +0200

    remove unused method

--- app/src/main/java/org/torproject/vpn/ui/bridgesettings/BridgeSettingsFragment.kt
+++ app/src/main/java/org/torproject/vpn/ui/bridgesettings/BridgeSettingsFragment.kt
@@ -3,16 +3,11 @@ package org.torproject.vpn.ui.bridgesettings
 import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.appcompat.widget.ViewUtils
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import org.torproject.vpn.R
-import org.torproject.vpn.circumvention.CircumventionApiManager
-import org.torproject.vpn.circumvention.SettingsRequest
 import org.torproject.vpn.databinding.FragmentBridgesettingsBinding
 import org.torproject.vpn.ui.bridgesettings.model.BridgeSettingsFragmentViewModel
 import org.torproject.vpn.utils.PreferenceHelper
@@ -83,40 +78,6 @@ class BridgeSettingsFragment: Fragment(R.layout.fragment_bridgesettings), ClickH
         if (key?.equals(PreferenceHelper.BRIDGE_TYPE) == true) {
             binding.rgBridgeOptions.check(viewModel.getSelectedBridgeTypeId())
         }
-    }
-
-    /*
-        This functionality is still very alpha. We're requesting bridge lines from the circumvention API,
-        but instead of evaluating the assumed device's country location, the country code of RU is hard coded
-        in order to enforce to receive actual results. For requested (presumably) uncensored locations an
-        empty array gets returned by the circumvention API. Moreover this feature doesn't use any
-        circumvention strategy itself for the API communication. Finally the returned bridge lines cannot be
-        used in reality to start tor, since PT support is not yet available. Only bridges without obfuscation
-        are currently supported.
-     */
-    private fun askTor () {
-        CircumventionApiManager().getSettings(SettingsRequest("ru"), {
-            it?.let { response ->
-                Log.d("result: ", "${response.settings}")
-                val results = mutableListOf<String>()
-                response.settings?.let { bridgesList ->
-                    for (bridges in bridgesList) {
-                        bridges.bridges.bridge_strings?.let { bridgeLines ->
-                            results.addAll(bridgeLines)
-                        }
-                    }
-                }
-                val dialog = BridgeDialogFragment.create(
-                    BridgeDialogFragmentArgs(
-                        argBridgeLines = results.toTypedArray()
-                    )
-                )
-                dialog.show(parentFragmentManager, "BRIDGE_DIALOG")
-            }
-        }, {
-            Log.e(TAG, "Ask Tor was not available... $it")
-            Toast.makeText(requireContext(),"Ask Tor was not available",Toast.LENGTH_LONG).show()
-        })
     }
 
 }
