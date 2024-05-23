5755c02157ee5981abac2e00c89308470fa903ef
ankit <ankitgusai21@gmail.com>
2024-05-22 22:49:02 -0400

    -Removed unused import.

--- app/src/main/java/org/torproject/vpn/ui/home/ConnectFragment.kt
+++ app/src/main/java/org/torproject/vpn/ui/home/ConnectFragment.kt
@@ -6,8 +6,6 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
