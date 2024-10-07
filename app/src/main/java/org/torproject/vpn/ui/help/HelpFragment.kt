6cde6403a6488db1a46d32a31b7fe6cc4ee2afda
cyBerta <cyberta@riseup.net>
2024-10-07 19:35:09 +0200

    remove unused import

--- app/src/main/java/org/torproject/vpn/ui/help/HelpFragment.kt
+++ app/src/main/java/org/torproject/vpn/ui/help/HelpFragment.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.content.Intent.ACTION_VIEW
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
