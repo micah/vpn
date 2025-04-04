cd4319ece159fdc4c8be78511b42b8fe7a2716f0
a-cyborg <gx_339-4@pm.me>
2025-04-04 20:39:08 +0900

    fix disconnecting VPN in system settings, auto-reconnect from Tor VPN

--- app/src/main/java/org/torproject/vpn/vpn/TorVpnServiceBinder.kt
+++ app/src/main/java/org/torproject/vpn/vpn/TorVpnServiceBinder.kt
@@ -1,6 +1,8 @@
 package org.torproject.vpn.vpn
 
 import android.os.Binder
+import android.os.IBinder
+import android.os.Parcel
 import org.torproject.onionmasq.ISocketProtect
 import java.lang.ref.WeakReference
 
@@ -8,6 +10,16 @@ class TorVpnServiceBinder(private val torVpnServiceReference: WeakReference<TorV
     override fun protect(socket: Int): Boolean {
         torVpnServiceReference.get()?.let {
             return it.protect(socket)
+        }
+        return false
+    }
+
+    override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
+        if (code == IBinder.LAST_CALL_TRANSACTION) {
+            torVpnServiceReference.get()?.let {
+                it.onRevoke()
+                return true
+            }
         }
         return false
     }
