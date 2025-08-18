4670795c88c0aea502bbb1333c717c70c98705b4
cyBerta <cyberta@riseup.net>
2025-08-18 19:06:12 +0200

    move md files from docs to doc directory

--- scripts/generate_html_from_md.sh
+++ scripts/generate_html_from_md.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 SCRIPT_DIR=$(dirname "$0")
 BASEDIR=$SCRIPT_DIR/..
 
-cd $BASEDIR/docs
+cd $BASEDIR/doc
 for i in *.md ; do
   echo "$i"
   pandoc -s $i --metadata title="$i" --css="file:///android_asset/help/layout.css" -o ../app/src/main/assets/help/$i.html ;
   sed -i '/<header id="title-block-header">/,/<\/header>/d' "../app/src/main/assets/help/$i.html"
-  done
+done
 cd --
